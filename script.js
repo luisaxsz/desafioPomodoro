e70d0853494f67d53ab6794fdba426dcacb4d2cc
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-05-07 17:40:27 -0300

    desafioPomodoro001

---
+++ script.js
@@ -0,0 +1,20 @@
+let minutes = 0;
+let seconds = 0;
+
+let cron;
+
+function start() {
+    cron = setInterval(timer, 1000)
+}
+function pause(){
+    cron = clearInterval(cron)
+}
+function timer(){
+    seconds++
+    if(seconds == 10){
+        minutes++
+        seconds = 0
+    }
+
+    document.getElementById("counter").innerText = `${minutes} : ${seconds}`
+}
