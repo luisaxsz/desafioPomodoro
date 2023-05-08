270e0caddfc9f44bf44f82849e152d9ff56b7372
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-05-08 18:50:24 -0300

    atualizacao003
    Atualizei o campo de tarefas, agora o usuário pode inserir as tarefas que ele busca fazer durante os ciclos dele e gerenciar através de uma to-do list.

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 let minutes = 0;
 let seconds = 0;
-
 let cron;
-
 function start() {
-    cron = setInterval(timer, 1000)
+    cron = setInterval(timer , 1000)
 }
 function pause(){
     cron = clearInterval(cron)
 }
 function timer(){
-    seconds++
+    seconds++ 
     if (seconds == 60){
         minutes++
         seconds = 0
     }
-
     document.getElementById("counter").innerText = `${minutes} : ${seconds}`
 }
+
+function addTask() {
+    let box = document.createElement('input')
+    box.setAttribute('type', 'checkbox')
+    let taskList = document.createElement('label')
+    let breakLine = document.createElement('br')
+    let task = document.getElementById('inputTask').value
+    let taskBox = document.getElementById('taskBox')
+    taskList.textContent = task
+
+
+    taskBox.appendChild(box)
+    taskBox.appendChild(taskList)
+    taskBox.appendChild(breakLine)
+
+    document.getElementById('inputTask').value = ""
+}
+
