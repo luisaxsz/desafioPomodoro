53087f698bf900620d5bb3ca722899c74502c82e
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-05-10 13:30:37 -0300

    atualizaçãoFinalFuncionalidades
    Criei a ultima funcionalidade que é dos ciclos de estudo do pomodoro e a inicialização deles automaticamente após o fim do outro como pedido no desafio da rafa ballerini.

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 let minutes = 0;
 let seconds = 0;
+let durations = [25,5,25,5,25,15];
+let currentDurationIndex = 0
 let cron;
 function start() {
-    cron = setInterval(timer , 1000)
+   cron = setInterval(timer,1000)
 }
+
 function pause(){
     cron = clearInterval(cron)
 }
+
 function timer(){
     seconds++ 
     if (seconds == 60){
         minutes++
         seconds = 0
     }
+
     document.getElementById("counter").innerText = `${minutes} : ${seconds}`
+   
+    const currentDuration = durations[currentDurationIndex];
+    const durationInSeconds = currentDuration * 60;
+  
+    if (minutes * 60 + seconds === durationInSeconds) {
+      currentDurationIndex++;
+      if (currentDurationIndex === durations.length) {
+        clearInterval(cron);
+        minutes = 0
+        seconds = 0 
+        currentDurationIndex = 0
+      }
+      minutes = 0;
+      seconds = 0;
+    }
+    
 }
 
 function addTask() {
